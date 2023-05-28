44bb80a4d545d1eb4097a2b28db2be6554cd695b
maghayei83 <127246933+maghayei83@users.noreply.github.com>
2023-05-28 04:05:00 -0700

    Add files via upload

---
+++ ccod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication51
+{
+    class Fire
+    {
+        int employeeid{set; get;}
+
+        date date{set; get;}
+
+        string reason{set; get}
+    }
+}
+class randomGenerator
+{
+
+    static void main()
+    {
+    DateTime startDate = new DateTime(2021, 1, 1); // replace with actual start date
+    DateTime endDate = new DateTime(2021, 1, 10); // replace with actual end date
+
+     TimeSpan duration = endDate - startDate;
+      int numberOfDays = duration.Days;
+
+      Console.WriteLine("Number of days: " + numberOfDays);
+    }
+}
